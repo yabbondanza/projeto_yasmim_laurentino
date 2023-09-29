b5f509f92f11eda2d8e6cb73aec290c353d3abab
Yasmim <yasmimabbondanza@gmail.com>
2023-09-29 19:26:51 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,28 @@
+public class C {
+    private String C1;
+    private int C2;
+
+    public String getC1() {
+        return C1;
+    }
+
+    public void setC1(String C1) {
+        this.C1 = C1;
+    }
+
+    public int getC2() {
+        return C2;
+    }
+
+    public void setC2(int C2) {
+        this.C2 = C2;
+    }
+
+    public void MC1() {
+        System.out.println("Método MC1() da classe C");
+    }
+
+    public void MC2() {
+        System.out.println("Método MC2() da classe C");
+    }
+}
