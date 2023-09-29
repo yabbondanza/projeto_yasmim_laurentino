54cd7a21dfac23c0db4ac3da4663c7d68436685a
Yasmim Abbondanza <78567347+yabbondanza@users.noreply.github.com>
2023-09-29 19:03:15 -0300

    adicionando arquivos iniciais

---
+++ B.java
@@ -0,0 +1,28 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    public int getB1() {
+        return B1;
+    }
+
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+
+    public void MB1() {
+        System.out.println("Método MB1() da classe B");
+    }
+
+    public void MB2() {
+        System.out.println("Método MB2() da classe B");
+    }
+}
