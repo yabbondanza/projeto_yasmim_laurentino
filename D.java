08cd05c97725c40e69e64b7a290017bb1b79ba99
Yasmim <yasmimabbondanza@gmail.com>
2023-09-29 21:48:08 -0300

    Adicionando a classe D

---
+++ D.java
@@ -0,0 +1,28 @@
+public class D {
+    private String D1;
+    private int D2;
+
+    public String getD1() {
+        return D1;
+    }
+
+    public void setD1(String D1) {
+        this.D1 = D1;
+    }
+
+    public int getD2() {
+        return D2;
+    }
+
+    public void setD2(int D2) {
+        this.D2 = D2;
+    }
+
+    public void MD1() {
+        System.out.println("Método MD1() da classe D");
+    }
+
+    public void MD2() {
+        System.out.println("Método MD2() da classe D");
+    }
+}
